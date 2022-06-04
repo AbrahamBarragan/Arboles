551bae23963de6d597949eabf5d43623592babe0
Abraham Barragan Lira <bala.barragan3@hotmail.com>
2022-06-04 14:26:15 -0700

    Cambios en arbolgeneral y en programs

--- ArbolesGenerales/ArbolGeneral.cs
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -25,12 +25,12 @@ namespace ArbolesGenerales
                 throw new Exception("El dato esta vacio");
             }
 
-            if(padre is null)
+            if (padre is null)
             {
                 throw new Exception("El padre no puede ser nulo");
             }
 
-            if(padre.Hijo is null)
+            if (padre.Hijo is null)
             {
                 padre.Hijo = new Nodo(dato);
                 return padre.Hijo;
@@ -38,7 +38,7 @@ namespace ArbolesGenerales
             else
             {
                 Nodo hijoActual = padre.Hijo;
-                while(hijoActual.Hermano is not null)
+                while (hijoActual.Hermano is not null)
                 {
                     hijoActual = hijoActual.Hermano;
                 }
@@ -46,5 +46,67 @@ namespace ArbolesGenerales
                 return hijoActual.Hermano;
             }
         }
+        private void Recorrer(Nodo nodo, ref int posicion, ref string datos)
+        {
+            if(nodo is not null)
+            {
+                string dato = nodo.Dato;
+                int CantidadGuiones = dato.Length + posicion;
+                string DatoConGuines = dato.PadLeft(CantidadGuiones, '-');
+                datos += $"{DatoConGuines}\n";
+
+                if (nodo.Hijo is not null)
+                {
+                    posicion++;
+                    Recorrer(nodo.Hijo, ref posicion, ref datos);
+                    posicion--;
+
+                }
+                if(nodo.Hermano is not null && posicion != 0)
+                {
+                    Recorrer(nodo.Hermano, ref posicion, ref datos);
+                }
+            }
+        }
+        public string ObtenerArbol(Nodo nodo = null)
+        {
+            if(nodo is null)
+            {
+                nodo = raiz;
+            }
+            int posicion = 0;
+            string datos = "";
+            Recorrer(nodo, ref posicion, ref datos);
+            return datos;
+        }
+        public Nodo Buscar(string dato, Nodo nodoBusqueda = null)
+        {
+            if(nodoBusqueda is null)
+            {
+                nodoBusqueda = raiz;
+            }
+            if(nodoBusqueda.Dato.ToUpper() == dato.ToUpper())
+            {
+                return nodoBusqueda;
+            }
+            if(nodoBusqueda.Hijo is not null)
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hijo);
+                if(nodoEncontrado is not null)
+                {
+                    return nodoEncontrado;
+                }
+            }
+            if(nodoBusqueda.Hermano is not null)
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hermano);
+                if(nodoEncontrado is not null)
+                {
+                    return nodoEncontrado;
+                }
+            }
+            return null;
+        }
     }
+    //investigar inmutavilidad
 }
